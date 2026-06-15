362b2a1a1ebec6291d236cd16d8f950f51cf8d20
USER1 <musictich2005@gmail.com>
2026-06-15 13:25:00 +0800

    feat(state): 中央狀態 + 粒子物件池

---
+++ src/state/gameState.js
@@ -0,0 +1,14 @@
+import { createEconomy } from '../systems/economy.js';
+import { BALANCE } from '../data/balance.js';
+
+export function createGameState(map) {
+  return {
+    map,
+    economy: createEconomy({ gold: BALANCE.startGold, lives: BALANCE.startLives }),
+    enemies: [], towers: [], projectiles: [],
+    occupiedSlots: new Set(),  // slot index 已建塔
+    wave: 0, waveTimer: 0, spawnQueue: [], spawnTimer: 0,
+    selectedTowerType: null, selectedTower: null,
+    now: 0, over: false, started: false,
+  };
+}
